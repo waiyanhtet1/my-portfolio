08e97386b3f9749b4103dbebcd6aa3eb67750a18
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2025-12-11 00:41:55 +0630

    added: 3d modal

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls, useDraco, useGltf, useGltfAnimations } from '@threlte/extras';
+
+	const dracoLoader = useDraco();
+	const gltf = useGltf('/assets/robot.glb', { dracoLoader });
+	const { actions } = useGltfAnimations<'Orange_black_sci_fi_unit2|A|2025_11_6_15_24_13'>(gltf);
+
+	$effect(() => {
+		$actions['Orange_black_sci_fi_unit2|A|2025_11_6_15_24_13']?.play();
+	});
+</script>
+
+<T.PerspectiveCamera makeDefault fov={20} position={[5, 0, 8]}>
+	<OrbitControls
+		autoRotate
+		autoRotateSpeed={10}
+		enableDamping
+		enableZoom={false}
+		target={[0, 1, 0]}
+		enableRotate={false}
+	/>
+</T.PerspectiveCamera>
+
+<!-- Add lighting here -->
+<T.AmbientLight intensity={1} />
+<T.DirectionalLight position={[10, 10, 5]} intensity={5} />
+<T.PointLight position={[-10, -10, -10]} intensity={5} />
+
+{#await gltf then { scene }}
+	<T is={scene} />
+{/await}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Scene from '$lib/components/Scene.svelte';
+	import { Canvas, T } from '@threlte/core';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Canvas>
+	<T.Color attach="background" args={['#000003']} />
+
+	<Scene />
+</Canvas>
